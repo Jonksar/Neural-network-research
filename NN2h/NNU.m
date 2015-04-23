a757ff5dbfa4181d20a988ddb6443a49e1900e47
Jonksar <Jonksar@git.com>
2015-04-23 14:52:35 +0300

    Everything should be up and running now. Modified such that NN2h/NNU.m is quite one to one with NN3h. [do; until] statement. Hard stop on training iterations.

--- NN2h/NNU.m
+++ NN2h/NNU.m
@@ -6,7 +6,7 @@ function [testdata, traindata] = NNU(X, Xtest, y, ytest, input)
     lambdam = [0.01, 0.1, 1, 10, 100];
     trainerror = zeros(numel(lambdam), 1);
     testerror = zeros(numel(lambdam), 1);
-    threshold = 1e-2;
+    threshold = 2e-3;
     lambda = lambdam(order_matrix(input, 3));
 
     % Defines what structure are we using
@@ -14,8 +14,6 @@ function [testdata, traindata] = NNU(X, Xtest, y, ytest, input)
     num_labels = 10;          % 10 labels, from 1 to 10
     hidden_layer_size = order_matrix(input, 1);
     hidden_layer_size2 = order_matrix(input, 2);
-
-
 
     % Useful variables
     m = size(X, 1);
@@ -32,7 +30,10 @@ function [testdata, traindata] = NNU(X, Xtest, y, ytest, input)
     pred = [];
     test_pred = [];
 
-    fprintf('\nInitializing Neural Network Parameters ...\n')
+    printf("Input %i defines network with Hidden layer size: %i 2nd Hls: %i Lambda: %i\n",
+                                            input, hidden_layer_size, hidden_layer_size2, lambda)
+
+    fprintf('\nGenerating initial Neural Network Parameters ...\n')
 
 
     nn_params = zeros(1, sizeT1 + sizeT2 + sizeT3);
@@ -40,13 +41,11 @@ function [testdata, traindata] = NNU(X, Xtest, y, ytest, input)
     initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
     initial_Theta2 = randInitializeWeights(hidden_layer_size, hidden_layer_size2);
     initial_Theta3 = randInitializeWeights(hidden_layer_size2, num_labels);
-
     % Unroll parameters
     initial_nn_params = [initial_Theta1(:); initial_Theta2(:); initial_Theta3(:)];
-    fprintf('\nFinished initializing...\n')
-    fprintf('\nTraining Neural Network... \n')
+    fprintf('\nTraining Neural Network...\n')
 
-    options = optimset('MaxIter', 80);
+    options = optimset('MaxIter', 120);
 
     % Create "short hand" for the cost function to be minimized
     costFunction = @(p) nnCostFunction2(p, ...
@@ -56,11 +55,21 @@ function [testdata, traindata] = NNU(X, Xtest, y, ytest, input)
                                        num_labels, X, y, lambda);
 
     % Now, costFunction is a function that takes in only one argument (the
-    % neural network parameters)
-    [nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+    % neural network parameters) train first with the initial parameters,
+    % save results to nn_params.
+    [cost, dummy] = costFunction(initial_nn_params);
 
-    % Reshape nn_params back to Theta1, Theta2, Theta3
+    nn_params = initial_nn_params;
 
+    i = 0;
+    do;
+
+    i += 1;
+    prev_cost = cost;
+    [nn_params, cost] = fmincg(costFunction, nn_params, options);
+
+
+    % Unrolling parameters
     Theta1 = reshape(nn_params(1:sizeT1), ...
                      hls, (ils + 1));
 
@@ -75,39 +84,12 @@ function [testdata, traindata] = NNU(X, Xtest, y, ytest, input)
 
     trainerror = mean(double(pred == y));
     testerror = mean(double(test_pred == ytest));
+    fprintf('\nTraining Set Accuracy: %f', trainerror * 100);
+    fprintf('\nTest Set Accuracy: %f\n', testerror * 100);
 
-    i = 0;
-    while (cost(1) - cost(numel(cost)) > threshold) & (i <= 20);
-        i += 1;
+    until(prev_cost - cost) < threshold || i < 20;
 
-        [nn_params, cost] = fmincg(costFunction, nn_params, options);
-
-        Theta1 = reshape(nn_params(1:sizeT1), ...
-                         hls, (ils + 1));
-
-        Theta2 = reshape(nn_params((sizeT1 + 1):(sizeT1+sizeT2)), ...
-                         hls2, (hls + 1));
-
-        Theta3 = reshape(nn_params((1 + sizeT1 + sizeT2):(sizeT1 + sizeT2 + sizeT3)), ...
-                         ols, (hls2 + 1));
-
-
-        pred = predict(Theta1, Theta2, Theta3, X);
-        test_pred = predict(Theta1, Theta2, Theta3, Xtest);
-
-
-        trainerror = mean(double(pred == y));
-        testerror = mean(double(test_pred == ytest));
-
-
-        fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
-        fprintf('\nTest Set Accuracy: %f\n', mean(double(test_pred == ytest)) * 100);
-        fprintf('\nTraining iteration: %i, Hidden layer size: %i 2nd Hls %i\n',
-                                            i, hidden_layer_size, hidden_layer_size2);
-
-    endwhile
-
-    save(sprintf("NN2h-%d-%d-L%d", hls, hls2, (order_matrix(input, 3)),
-         "trainerror", "testerror");
+    printf("Saving file %s\n", sprintf("NN2h-%d-%d-L%d", hls, hls2, order_matrix(input, 3)))
+    save(sprintf("NN2h-%d-%d-L%d", hls, hls2, order_matrix(input, 3)), "trainerror", "testerror")
 
 endfunction
